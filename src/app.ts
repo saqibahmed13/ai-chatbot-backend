547ebf2c3bc912ad6622e8c495792679781f4100
saqibahmed13 <saqiba0484@gmail.com>
2024-01-10 21:34:08 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import express, { response } from "express";
+import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { request } from "http";
+config();
+const app = express();
+
+//middlewares
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"]
+};
+
+// Middlewares
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//remove it in production
+app.use(morgan("dev"));
+let x=11;
+app.use("/api/v1", appRouter);
+
+// app.get("/",(request,response,next)=>{
+//     if(x>10){
+//         response.json("hello world")
+//     }
+//     else{
+//         response.json("jaa ba soja")
+//     }
+   
+// })
+
+export default app;
